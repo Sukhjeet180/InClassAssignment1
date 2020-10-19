875bc93d9cd85b7203e0a6811816ccf405c50b3c
Sukhjeet180 <sukhjeetdhanoa180@gmail.com>
2020-10-18 22:54:25 -0400

    First commit

---
+++ FirstInClassAssignment/FirstInClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FirstInClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers:");
+            string input = Console.ReadLine();
+            string[] arr = input.Split('+');
+            int sum = 0;
+
+            try
+            {
+                foreach (string x in arr)
+                    sum += int.Parse(x);
+
+                Console.WriteLine("Sum of the input elements is {0}", sum);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Incorrect Input");
+            }
+        }
+    }
+}
